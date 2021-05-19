ca896a3a066ec357ef47020c5670027106be81de
Robert-zar <78647659+Robert-zar@users.noreply.github.com>
2021-05-20 00:52:59 +0300

    Update predicates.pl

--- lab1/predicates.pl
+++ lab1/predicates.pl
@@ -15,6 +15,7 @@ father(X):-parent(Y,X),man(Y),write(Y).
 
 % task 1.5
 brother(X,Y):-parent(Z,Y),parent(Z,X),man(Z),man(X),not(X=Y)
+brothers(X):-brother(Y,X),write(Y),nl,fail.
 
 % task 1.6
 wife(X,Y):-parent(Y,Z),parent(X,Z),woman(X).
@@ -22,9 +23,11 @@ wife(X):-man(X),wife(Y,X),write(Y).
 
 % task 1.7
 b_s(X,Y):-parent(Z,X),parent(Z,Y),woman(Z),not(X=Y).
+b_s(X):-b_s(X,Y),write(Y),nl,fail.	
 
 % task 1.8
 grand_pa(X,Y):-parent(Z,Y),parent(X,Z),man(X).
+grand_pas(X):-grand_pa(Y,X),write(Y),nl,fail.
 
 % task 1.9
 grand_ma(X,Y):-parent(Z,Y),parent(X,Z),woman(X).
@@ -34,13 +37,14 @@ grand_mas(X):-grand_ma(Y,X),write(Y),nl,fail.
 grand_pa_and_son(X,Y):-grand_pa(X,Y),man(Y).
 grand_pa_and_son(X,Y):-grand_pa(Y,X),man(X).
 
-
 % task 1.11
 grand_ma_and_son(X,Y):-parent(X,Z),parent(Z,Y),woman(X),man(Y).
 grand_ma_and_son(X,Y):-parent(Y,Z),parent(Z,X),man(X),woman(Y).
 
 % task 1.12
 uncle(X,Y):-parent(Z,Y),brother(X,Z).
+uncles(X):-uncle(Y,X),write(Y),nl,fail.
 
 % task 1.13
 newphew(X,Y):-man(X),uncle(Y,X).
+newphews(X):-newphew(Y,X),write(Y),nl,fail.
