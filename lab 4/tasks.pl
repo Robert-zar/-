feb80b4e194ed01f925206c73f4c2bd38d86770d
Robert-zar <78647659+Robert-zar@users.noreply.github.com>
2021-07-15 15:01:08 +0300

    Update tasks.pl

--- lab 4/tasks.pl
+++ lab 4/tasks.pl
@@ -9,3 +9,8 @@ append_list(CurL, [X], CurL1), read_list(N, CurN1, CurL1, L).
 
 write_list([]) :- !.
 write_list([H|T]) :- write(H), write(" "), write_list(T).
+
+% task 2
+sum_list_down([],Sum,Sum):-!.
+sum_list_down([H|T],CurSum,Sum):- CurSum1 is CurSum + H, sum_list_down(T, CurSum1, Sum).
+sum_list_down([H|T], Sum):- sum_list_down([H|T], 0, Sum).
