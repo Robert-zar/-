e031fa38f049c418b9d70c055d05bfd9c90dfd64
Robert-zar <78647659+Robert-zar@users.noreply.github.com>
2021-07-15 17:41:06 +0300

    Update tasks.pl

--- lab 4/tasks.pl
+++ lab 4/tasks.pl
@@ -22,3 +22,23 @@ sum_list_up([H|T], Sum) :- sum_list_up(T, Sum1), Sum is Sum1 + H.
 % task 6
 min_list_up([H],H):-!.
 min_list_up([H|T],Min):-min_list_up(T,Min1), (H < Min1 -> Min is H;Min is Min1).
+
+% task 7
+min_list_down([],CurMin,CurMin):-!.
+min_list_down([H|T],CurMin,Min):-(H < CurMin -> CurMin1 is H; CurMin1 is CurMin),min_list_down(T,CurMin1,Min).
+min_list_down([H|T],Min):- min_list_down(T,H,Min).
+
+% task 9
+member([X|_],X):-!.
+member([_|T],X):-member(T,X).
+
+% task 10
+reverse([],CurList,CurList):-!.
+reverse([H|T],CurList,NewList):-reverse(T,[H|CurList],NewList).
+reverse(L,NewList):-reverse(L,[],NewList).
+
+% task 11
+p([],_):-!.
+p(_,[]):-fail,!.
+p([H|Sub_list],[H|List]):-p(Sub_list,List),!.
+p(Sub_list,[_|List]):-p(Sub_list,List).
