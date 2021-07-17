c273debf892fafec3f7a89a144756dfcaca4dd1e
Robert-zar <78647659+Robert-zar@users.noreply.github.com>
2021-07-17 17:20:07 +0300

    Update tasks.pl

--- lab 5/tasks.pl
+++ lab 5/tasks.pl
@@ -26,3 +26,21 @@ el_no(List,Num,El):-el_no(List,Num,1,El).
 el_no([H|_],Num,Num,H):-!.
 el_no([_|Tail],Num,Ind,El):-Ind1 is Ind+1,el_no(Tail,Num,Ind1,El).
 
+pr_ein:- Houses=[_,_,_,_,_],
+
+		in_list(Houses,[red,english,_,_,_]),
+		in_list(Houses,[_,spanish,_,dog,_]),
+		in_list(Houses,[green,_,coffee,_,_]),
+		in_list(Houses,[_,ukrain,tea,_,_]),
+		sprava_next([green,_,_,_,_],[white,_,_,_,_],Houses),
+		in_list(Houses,[_,_,_,ulitka,old_gold]),
+		in_list(Houses,[yellow,_,_,_,kool]),
+		el_no(Houses,3,[_,_,milk,_,_]),
+		el_no(Houses,1,[_,norway,_,_,_]),
+		next_to([_,_,_,_,chester],[_,_,_,fox,_],Houses),
+		next_to([_,_,_,_,kool],[_,_,_,horse,_],Houses),
+		in_list(Houses,[_,_,orange,_,lucky]),
+		in_list(Houses,[_,japan,_,_,parlament]),
+		next_to([_,norway,_,_,_],[blue,_,_,_,_],Houses),
+
+
